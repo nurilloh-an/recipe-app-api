957b6c7b4977963155d966ae607c242ba35ac36f
nurilloh2004 <nurillohpython@gmail.com>
2022-11-22 21:45:27 +0500

    all

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,8 +75,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
