79672865be7344f1dc0077eac2750b58375973cc
nurilloh2004 <nurillohpython@gmail.com>
2022-11-23 14:23:17 +0500

    added wait_for_db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -21,8 +21,7 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
-    
-    
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
