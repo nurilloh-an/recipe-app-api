6a79aea3fc2128c364c31b68d37b534da83ea27e
nurilloh2004 <nurillohpython@gmail.com>
2022-11-24 09:05:12 +0500

    add some tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,6 @@
+"""
+Tests for models.
+"""
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -6,11 +9,12 @@ class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password,
+            email=email,
+            password=password,
         )
 
         self.assertEqual(user.email, email)
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -12,6 +12,7 @@ from rest_framework import status
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 
+
 def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
