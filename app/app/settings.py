73fcaec3dc891ff89e34662eb311415ff7fdcd47
nurilloh2004 <nurillohpython@gmail.com>
2022-11-24 08:17:59 +0500

    test for user

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'rest_framework',
     'drf_spectacular',
-    'core',
+    'user',
 ]
 
 MIDDLEWARE = [
